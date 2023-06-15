98ebbae9cbcd925c3cc99361eff5fc7db50258de
Brenolinhares <89438448+Brenolinhares@users.noreply.github.com>
2023-06-15 17:50:30 -0300

    Add files via upload

---
+++ PROJETO_4/the_robot_2 (1).pml
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<Package name="the_robot_2 (1)" format_version="5">
+    <Manifest src="manifest.xml" />
+    <BehaviorDescriptions>
+        <BehaviorDescription name="behavior" src="the_robot_2 (1)" xar="behavior.xar" />
+        <BehaviorDescription name="behavior" src="tocar" xar="behavior.xar" />
+    </BehaviorDescriptions>
+    <Dialogs>
+        <Dialog name="ExampleDialog" src="the_robot_2 (1)/ExampleDialog/ExampleDialog.dlg" />
+    </Dialogs>
+    <Resources>
+        <File name="title" src="title.mp3" />
+        <File name="anunciacao_sax3" src="anunciacao_sax3.mp3" />
+        <File name="epicsax" src="the_robot_2 (1)/epicsax.ogg" />
+    </Resources>
+    <Topics>
+        <Topic name="ExampleDialog_enu" src="the_robot_2 (1)/ExampleDialog/ExampleDialog_enu.top" topicName="ExampleDialog" language="en_US" nuance="enu" />
+    </Topics>
+    <IgnoredPaths />
+    <Translations>
+        <Translation name="translation_en_US" src="translations/translation_en_US.ts" language="en_US" />
+        <Translation name="translation_pt_PT" src="translations/translation_pt_PT.ts" language="pt_PT" />
+    </Translations>
+</Package>
---
+++ PROJETO_4/translations/translation_pt_PT.ts
@@ -0,0 +1,170 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pt_PT">
+    <context>
+        <name>the_robot_2 (1)/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Olá</translation>
+        </message>
+        <message>
+            <location filename="the_robot_2 (1)/behavior.xar" line="0"/>
+            <source>Sei, sim. Vou tocar uma musica na guitarra</source>
+            <comment>Text</comment>
+            <translation type="unfinished"></translation>
+        </message>
+    </context>
+    <context>
+        <name>the_robot_2 (1)/behavior.xar:/Choice</name>
+        <message>
+            <source>no/no nao/disagree/I disagree</source>
+            <comment>SpeechReco negative</comment>
+            <translation type="vanished">não/não NAO/não é isso/não creio</translation>
+        </message>
+        <message>
+            <source>yes/yes nao/agree/I agree/of course</source>
+            <comment>SpeechReco positive</comment>
+            <translation type="vanished">sim/sim NAO/OK/exatamente</translation>
+        </message>
+        <message>
+            <source>help/help me/i don't know</source>
+            <comment>SpeechReco help</comment>
+            <translation type="vanished">a ajuda/ajuda-me/ajuda/não sei/podes ajudar-me?/o que faço?/o que posso fazer?/como posso fazer?</translation>
+        </message>
+        <message>
+            <source>exit/stop/quit</source>
+            <comment>SpeechReco exit</comment>
+            <translation type="vanished">sair/passar/pára/anular/fechar</translation>
+        </message>
+        <message>
+            <source>repeat/pardon/what/excuse me</source>
+            <comment>SpeechReco repeat</comment>
+            <translation type="vanished">repetir/repita/desculpe?/o quê?/como?</translation>
+        </message>
+        <message>
+            <source>I understood %s. Is that correct?</source>
+            <comment>TTS confirmation</comment>
+            <translation type="vanished">Entendi %s. Está correto?</translation>
+        </message>
+        <message>
+            <source>, / or </source>
+            <comment>TTS enumMarks</comment>
+            <translation type="vanished">, / ou </translation>
+        </message>
+        <message>
+            <source>No answer is really expected. /Your answer can be %s. /Your answer can be for example %s. /%s?</source>
+            <comment>TTS helpEnumChoices</comment>
+            <translation type="vanished">Não se espera uma resposta fixa. /Podes responder %s. /Podes responder por exemplo %s. /%s?</translation>
+        </message>
+        <message>
+            <source> you can ask me: %s. </source>
+            <comment>TTS helpEnumDefault</comment>
+            <translation type="vanished"> Podes pedir-me: %s. </translation>
+        </message>
+        <message>
+            <source>You can also use my tactile sensor to choose the answer./Pau=600 Press the front or the rear of my skull cap to go through all of the available answers. Press then the circle in the middle to validate your choice.</source>
+            <comment>TTS helpTactile</comment>
+            <translation type="vanished">Também podes escolher uma resposta através do meu sensor táctil./\Pau=600\ Carrega na parte da frente ou de trás da minha cúpula para navegar entre as possíveis respostas. De seguida, carrega na circunferência do meio para validar a tua opção.</translation>
+        </message>
+        <message>
+            <source>I did not understand. </source>
+            <comment>TTS notUnderstood</comment>
+            <translation type="vanished">Não entendi. </translation>
+        </message>
+        <message>
+            <source>No question has been defined, so I cannot repeat it. </source>
+            <comment>TTS noQuestion</comment>
+            <translation type="vanished">Não há perguntas predefinidas, portanto não há nada que eu possa repetir. </translation>
+        </message>
+        <message>
+            <source>I did not understand. Anims</source>
+            <comment>TTS notUnderstoodAnims</comment>
+            <translation type="vanished">Há barulho demais aqui! /Tente falar comigo mais claramente. /Não te ouço muito bem. </translation>
+        </message>
+    </context>
+    <context>
+        <name>the_robot_2 (1)/behavior.xar:/Choice (1)</name>
+        <message>
+            <source>no/no nao/disagree/I disagree</source>
+            <comment>SpeechReco negative</comment>
+            <translation type="vanished">não/não NAO/não é isso/não creio</translation>
+        </message>
+        <message>
+            <source>yes/yes nao/agree/I agree/of course</source>
+            <comment>SpeechReco positive</comment>
+            <translation type="vanished">sim/sim NAO/OK/exatamente</translation>
+        </message>
+        <message>
+            <source>help/help me/i don't know</source>
+            <comment>SpeechReco help</comment>
+            <translation type="vanished">a ajuda/ajuda-me/ajuda/não sei/podes ajudar-me?/o que faço?/o que posso fazer?/como posso fazer?</translation>
+        </message>
+        <message>
+            <source>exit/stop/quit</source>
+            <comment>SpeechReco exit</comment>
+            <translation type="vanished">sair/passar/pára/anular/fechar</translation>
+        </message>
+        <message>
+            <source>repeat/pardon/what/excuse me</source>
+            <comment>SpeechReco repeat</comment>
+            <translation type="vanished">repetir/repita/desculpe?/o quê?/como?</translation>
+        </message>
+        <message>
+            <source>I understood %s. Is that correct?</source>
+            <comment>TTS confirmation</comment>
+            <translation type="vanished">Entendi %s. Está correto?</translation>
+        </message>
+        <message>
+            <source>, / or </source>
+            <comment>TTS enumMarks</comment>
+            <translation type="vanished">, / ou </translation>
+        </message>
+        <message>
+            <source>No answer is really expected. /Your answer can be %s. /Your answer can be for example %s. /%s?</source>
+            <comment>TTS helpEnumChoices</comment>
+            <translation type="vanished">Não se espera uma resposta fixa. /Podes responder %s. /Podes responder por exemplo %s. /%s?</translation>
+        </message>
+        <message>
+            <source> you can ask me: %s. </source>
+            <comment>TTS helpEnumDefault</comment>
+            <translation type="vanished"> Podes pedir-me: %s. </translation>
+        </message>
+        <message>
+            <source>You can also use my tactile sensor to choose the answer./Pau=600 Press the front or the rear of my skull cap to go through all of the available answers. Press then the circle in the middle to validate your choice.</source>
+            <comment>TTS helpTactile</comment>
+            <translation type="vanished">Também podes escolher uma resposta através do meu sensor táctil./\Pau=600\ Carrega na parte da frente ou de trás da minha cúpula para navegar entre as possíveis respostas. De seguida, carrega na circunferência do meio para validar a tua opção.</translation>
+        </message>
+        <message>
+            <source>I did not understand. </source>
+            <comment>TTS notUnderstood</comment>
+            <translation type="vanished">Não entendi. </translation>
+        </message>
+        <message>
+            <source>No question has been defined, so I cannot repeat it. </source>
+            <comment>TTS noQuestion</comment>
+            <translation type="vanished">Não há perguntas predefinidas, portanto não há nada que eu possa repetir. </translation>
+        </message>
+        <message>
+            <source>I did not understand. Anims</source>
+            <comment>TTS notUnderstoodAnims</comment>
+            <translation type="vanished">Há barulho demais aqui! /Tente falar comigo mais claramente. /Não te ouço muito bem. </translation>
+        </message>
+    </context>
+    <context>
+        <name>the_robot_2 (1)/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Olá</translation>
+        </message>
+    </context>
+    <context>
+        <name>the_robot_2 (1)/behavior.xar:/Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Olá</translation>
+        </message>
+    </context>
+</TS>
